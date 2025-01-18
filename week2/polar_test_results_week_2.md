bcf0924674151e5b84b7b587126fa1e8afd94cb0
joshualeeee <102551497+joshualeeee@users.noreply.github.com>
2025-01-17 23:50:42 -0800

    polar results

---
+++ week2/polar_test_results_week_2.md
@@ -0,0 +1,22 @@
+# Week 2 Results - polar
+
+## 1-Hour Timeframe
+
+- **Timeframe:** 2022-04-04 00 to 2022-04-04 01
+- **Execution Time:** 48140.148084 ms
+- **Most Placed Color:** #000000
+- **Most Placed Pixel Location:** 0,0
+
+## 3-Hour Timeframe
+
+- **Timeframe:** 2022-04-04 00 to 2022-04-04 03
+- **Execution Time:** 50552.654250 ms
+- **Most Placed Color:** #000000
+- **Most Placed Pixel Location:** 0,0
+
+## 6-Hour Timeframe
+
+- **Timeframe:** 2022-04-04 00 to 2022-04-04 06
+- **Execution Time:** 52557.664000 ms
+- **Most Placed Color:** #000000
+- **Most Placed Pixel Location:** 0,0
---
+++ week2/polars_analyzer.py
@@ -0,0 +1,88 @@
+import sys
+from datetime import datetime
+import time
+from collections import Counter
+import polars as pl
+
+def checkDates(startDate, endDate):
+    try:
+        datetime.strptime(startDate, "%Y-%m-%d %H")
+    except ValueError:
+        print(f"Error: '{startDate}' is not in the correct format 'YYYY-MM-DD HH'.")
+        sys.exit(1)
+
+    try:
+        datetime.strptime(endDate, "%Y-%m-%d %H")
+    except ValueError:
+        print(f"Error: '{endDate}' is not in the correct format 'YYYY-MM-DD HH'.")
+        sys.exit(1)
+
+    if endDate <= startDate:
+        print("Error: End date must be after the start date.")
+        sys.exit(1) 
+    
+    return startDate, endDate
+
+def polars(startDate, endDate):
+    startDate = datetime.strptime(startDate, "%Y-%m-%d %H").strftime("%Y-%m-%d %H:%M:%S")
+    endDate = datetime.strptime(endDate, "%Y-%m-%d %H").strftime("%Y-%m-%d %H:%M:%S")
+
+    # Connect to DuckDB (in-memory or persistent)
+    lazy_df = pl.scan_csv("../../2022_place_canvas_history.csv")
+
+    filtered = (
+        lazy_df
+        .filter((pl.col("timestamp") >= pl.lit(startDate)) & (pl.col("timestamp") <= pl.lit(endDate)))
+        .collect()  # Execute and load into memory only after filtering
+    )
+
+    pixel_color = (
+        filtered
+        .group_by("pixel_color")  # Group by `pixel_color`
+        .len()  # Count occurrences
+        .sort("len", descending=True)  # Sort by count in descending order
+        .select("pixel_color")
+        .head(1)  # Take the first row (most frequent)
+    )[0, "pixel_color"]
+
+    coordinate = (
+        filtered
+        .group_by("coordinate")  # Group by `coordinate`
+        .len()  # Count occurrences
+        .sort("len", descending=True)  # Sort by count in descending order
+        .select("coordinate")
+        .head(1)  # Take the first row (most frequent)
+    )[0, "coordinate"]
+    
+    # Perform the same grouping and aggregation as above on `result`
+    return pixel_color, coordinate
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: analyzer.py <start_date> <end_date>")
+        sys.exit(1)
+
+    # Extract command-line arguments
+    start_date_str = sys.argv[1]
+    end_date_str = sys.argv[2]
+
+    # Validate and start and end date
+    startDate, endDate = checkDates(start_date_str, end_date_str)
+    
+    # Start the timer
+    startTime = time.perf_counter_ns()
+
+    color, coord = polars(startDate, endDate)
+
+    # End the timer
+    endTime = time.perf_counter_ns()
+
+    # Calculate elapsed time in seconds
+    elapsedTime_ns = endTime - startTime
+    elapsedTime_ms = elapsedTime_ns / 1_000_000
+    
+    print(f"**Timeframe:** {start_date_str} to {end_date_str}")
+    print(f"**Execution Time:** {elapsedTime_ms:.6f} ms")
+    print(f"**Most Placed Color:** {color} ")
+    print(f"**Most Placed Pixel Location:** {coord} ")
+
